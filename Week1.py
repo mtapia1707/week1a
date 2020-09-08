015240a73f0d4e817490a6ca19ef0ea0e5236d34
Michelle <mtapia@student.bridgew.edu>
2020-09-08 12:20:59 -0400

    hi

---
+++ Week1.py
@@ -0,0 +1,10 @@
+print("Hello Michelle")
+print("comp 151")
+age = 5
+# this is a number
+dog_years = 7 * age
+print(f"The dog is {dog_years}")
+fake_years = "123"  #this is not a number
+print(f"kske years is {fake_years}")
+fake_years_2 = fake_years * 7
+print(f"fake years 2 is {fake_years_2}")
